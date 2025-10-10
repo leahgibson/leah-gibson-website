8f9c79d04b739b2ad4d2494c25b34d7400b8eb41
leahgibson <ldgibson@rams.colostate.edu>
2025-10-10 16:48:24 -0600

    coming soon!

--- app/software-engineering/page.js
+++ app/software-engineering/page.js
@@ -9,7 +9,7 @@ export default function SoftwareEngineering() {
         <div className="max-w-4xl mx-auto">
             <h1 className="text-4xl font-bold mb-6 text-gray-800">Software</h1>
             <p className="text-lg text-gray-700">
-            Your data science portfolio content goes here...
+            Portfolio coming soon!
             </p>
         </div>
         </main>
